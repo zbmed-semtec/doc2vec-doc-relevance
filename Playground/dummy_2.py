344209fba96d210d46324af9fb9d74b108c348d5
talhamohsin <talhamohsin50@gmail.com>
2022-05-13 19:42:20 +0200

    Dummy code added to playground

---
+++ Playground/dummy_2.py
@@ -0,0 +1,38 @@
+#Import all the dependencies
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+
+data = ["I love machine learning. Its awesome.",
+        "I love coding in python",
+        "I love building chatbots",
+        "they chat amagingly well"]
+
+tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
+
+print (tagged_data)
+
+max_epochs = 100
+vec_size = 20
+alpha = 0.025
+
+model = Doc2Vec(vector_size=vec_size,
+                alpha=alpha, 
+                min_alpha=0.00025,
+                min_count=1,
+                epochs=5,
+                dm =1)
+  
+model.build_vocab(tagged_data)
+
+for epoch in range(max_epochs):
+    print('iteration {0}'.format(epoch))
+    model.train(tagged_data,
+                total_examples=model.corpus_count,
+                epochs=model.epochs)
+    # decrease the learning rate
+    model.alpha -= 0.0002
+    # fix the learning rate, no decay
+    model.min_alpha = model.alpha
+
+model.save("Playground/d2v.model")
+print("Model Saved")
