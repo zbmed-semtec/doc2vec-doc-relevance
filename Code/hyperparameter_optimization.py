ac5540e3e108b7e367010391d11cf56646eea8c3
talhamohsin <talhamohsin50@gmail.com>
2022-08-07 20:18:39 +0200

    Get parameter and update tsv file for the first AUC = 0

--- Code/hyperparameter_optimization.py
+++ Code/hyperparameter_optimization.py
@@ -42,48 +42,71 @@ def save_file(hp_df: pd.DataFrame, output_path: str) -> None:
     output_path: str
         File path where the tsv file will be saved.
     """
-    hp_df['AUC'] = 0
+    hp_df['AUC'] = 0.0
     hp_df.to_csv(output_path, sep="\t", index=False)
 
-def get_parameters(input_file: str, index: int) -> dict:
+def get_parameters(input_file: str) -> dict:
     """
-    Retrieve the set of parameters at a particular index 
-    to be used in the Doc2Vec model.
+    Retrieve the set of parameters for the first row in the 
+    tsv file with "AUC" value = 0 which will be used in the 
+    Doc2Vec model.
 
     Parameters
     ----------
     input_file: str
         TSV file containing all the combinations of the 
         hyperparameters.
-    index: int
-        Index from which the Doc2Vec parameters are to 
-        be retrieved.
+    Returns
+    -------
+    params: dict
+        Dictionary containing the parameters for the first 
+        row in the tsv file with "AUC" value = 0
     """
     hp_df = pd.read_csv(input_file, delimiter="\t")
-    df = hp_df.iloc[index]
+
+    # Check if any AUC value = 0 exists
+    if (hp_df.loc[hp_df['AUC'] == 0].index.tolist()):
+        # First row with AUC value = 0
+        df = hp_df[hp_df['AUC'] == 0].iloc[0]
+    # Else return the first row
+    else:
+        df = hp_df.iloc[0]
+
     params = df.to_dict()
     
     # Remove AUC from the dictionary
     del params["AUC"]
 
+    # Convert type of dict values from float to int
+    for k, v in params.items():
+        params[k] = int(v)
+
     return params
 
-def update_file(file_path: str, index: int, AUC_value: float) -> None:
+def update_file(file_path: str, AUC_value: float) -> None:
     """
-    Updates the AUC value in the tsv file at the specified index.
+    Updates the AUC value in the tsv file for the first 
+    row with AUC value = 0.
 
     Parameters
     ----------
     file_path: str
         File path of the tsv file which will be updated.
-    index: int
-        Index at which the AUC value will be updated.
     AUC_value: float
         AUC value which will be stored.
     """
     hp_df = pd.read_csv(file_path, delimiter="\t")
-    hp_df.at[index, "AUC"] = AUC_value
-    hp_df.to_csv(file_path, sep="\t", index=False)
+
+    # hp_df.at[0, "AUC"] = AUC_value
+    # hp_df.to_csv(file_path, sep="\t", index=False)
+    
+    # Check if any AUC value = 0 exists
+    if (hp_df.loc[hp_df['AUC'] == 0].index.tolist()):
+        # Get first index of row with AUC value = 0
+        index = hp_df.loc[hp_df['AUC'] == 0].index[0]
+
+        hp_df.at[index, "AUC"] = AUC_value
+        hp_df.to_csv(file_path, sep="\t", index=False)
 
 # Results in 18 combinations
 params_d2v = {
@@ -93,4 +116,3 @@ params_d2v = {
     "min_count": [1], 
     "epochs": [5], 
     "workers": [8]}
-
